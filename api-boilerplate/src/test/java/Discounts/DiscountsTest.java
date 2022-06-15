95850bd471fd0272ec6520c7fee5d24172db277f
Miguel Oliveira <oliveirasmj@gmail.com>
2022-06-15 14:59:35 +0100

    Create unit tests

--- api-boilerplate/src/test/java/Discounts/DiscountsTest.java
+++ api-boilerplate/src/test/java/Discounts/DiscountsTest.java
@@ -7,14 +7,28 @@ import static org.testng.Assert.*;
 public class DiscountsTest {
 
     @Test
-    public void testDiscountByAge() {
+    public void testDiscountByAge_GreaterThanOrEqual5() {
+        assertEquals(Discounts.discountByAge(2017), 2); //5anos
+    }
+
+    @Test
+    public void testDiscountByAge_GreaterThanOrEqual5AndLessThan10() {
+        assertEquals(Discounts.discountByAge(2016), 3); //6 anos
+    }
+
+    @Test
+    public void testDiscountByAge_GreaterThan10() {
+        System.out.println(11/2);
+        assertEquals(Discounts.discountByAge(2011), 6); //11 anos
     }
 
     @Test
     public void testDiscountByBrand() {
+
     }
 
     @Test
     public void testCalculateTotalDiscount() {
+
     }
 }
