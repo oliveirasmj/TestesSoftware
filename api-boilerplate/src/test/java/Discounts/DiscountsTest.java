96df0e87cd263e122f94c926d76f7fd6f07401aa
Miguel Oliveira <oliveirasmj@gmail.com>
2022-06-16 10:18:12 +0100

    add calculateTotalDiscount

--- api-boilerplate/src/test/java/Discounts/DiscountsTest.java
+++ api-boilerplate/src/test/java/Discounts/DiscountsTest.java
@@ -29,8 +29,20 @@ class DiscountsTest {
     }
 
     @ParameterizedTest
-    @CsvSource({"VW,3", "Tesla,5", "BMW,7", "Mercedes,0"})
-    void calculateTotalDiscount() {
+    @CsvSource({
+            "5, VW, 2022-06-16, 5", //idade 5 -> desconto 2; VW -> desconto 3; 2022-06-16(quinta-feira) -> desconto = 2+3 = 5 (MAX = nao atingido)
+            "50, VW, 2022-06-16, 25", //idade 50 -> desconto 26; VW -> desconto 3; 2022-06-16(quinta-feira) -> desconto = 26+3 = 29 (MAX = 25)
+            "50, VW, 2022-06-17, 25", //idade 50 -> desconto 26; VW -> desconto 3; 2022-06-17(sexta-feira) -> desconto = 26+3+5 = 34 (MAX = 25)
+            "5, VW, 2022-06-17, 10" //idade 5 -> desconto 2; VW -> desconto 3; 2022-06-17(sexta-feira) -> desconto = 2+3+5 = 10 (MAX = nao atingido)
+    })
 
+    void calculateTotalDiscount(int age, String brand, String todayString, int expected) {
+        int actualYear = LocalDate.now().getYear(); //ano atual
+        int carYear = actualYear - age; //idade carro
+        LocalDate today = LocalDate.parse(todayString);
+
+        //Chamar funcao
+        int discount = Discounts.calculateTotalDiscount(carYear, brand, today); //obter desconto
+        assertEquals(expected, discount);
     }
 }
