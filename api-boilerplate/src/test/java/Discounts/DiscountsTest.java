283e452e7d8c3d57021ca17681b6eb76c77dfeab
Miguel Oliveira <oliveirasmj@gmail.com>
2022-06-16 09:40:54 +0100

    discountByAge and discountByBrand tests

--- api-boilerplate/src/test/java/Discounts/DiscountsTest.java
+++ api-boilerplate/src/test/java/Discounts/DiscountsTest.java
@@ -1,34 +1,36 @@
 package Discounts;
 
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.testng.Assert.*;
+import java.time.LocalDate;
 
-public class DiscountsTest {
+import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.assertEquals;
 
-    @Test
-    public void testDiscountByAge_GreaterThanOrEqual5() {
-        assertEquals(Discounts.discountByAge(2017), 2); //5anos
+class DiscountsTest {
+    final int x = 2+10-3;
+
+    @ParameterizedTest
+    @CsvSource({"-5,0", "0,0", "4,0", "5,2", "6,5", "8,7", "9,8", "10,2", "11,6"})
+    void discountByAge(int age, int expected) {
+        int actualYear = LocalDate.now().getYear(); //ano atual
+        int yearCar = actualYear - age; //idade carro
+        int discount = Discounts.discountByAge(yearCar); //obter desconto
+        assertEquals(expected, discount);
     }
 
-    @Test
-    public void testDiscountByAge_GreaterThanOrEqual5AndLessThan10() {
-        assertEquals(Discounts.discountByAge(2016), 3); //6 anos
+    @ParameterizedTest
+    @CsvSource({"VW,3", "Tesla,5", "BMW,7", "Mercedes,0"})
+    void discountByBrand(String brand, int expected) {
+        int discount = Discounts.discountByBrand(brand); //obter desconto
+        assertEquals(expected, discount);
     }
 
-    @Test
-    public void testDiscountByAge_GreaterThan10() {
-        System.out.println(11/2);
-        assertEquals(Discounts.discountByAge(2011), 6); //11 anos
-    }
-
-    @Test
-    public void testDiscountByBrand() {
-
-    }
-
-    @Test
-    public void testCalculateTotalDiscount() {
+    @ParameterizedTest
+    @CsvSource({"VW,3", "Tesla,5", "BMW,7", "Mercedes,0"})
+    void calculateTotalDiscount() {
 
     }
 }
